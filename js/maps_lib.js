3a5de5dd5f6b807a29bf749546c2f3cd05e0f56f
yanrigidw <47276715+yanrigidw@users.noreply.github.com>
2019-02-09 19:27:13 +0700

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -178,6 +178,12 @@
         self.whereClause = self.locationColumn + " not equal to ''";
         
         //-----custom filters-----
+        var type_column = "'type'";
+        var searchType = type_column + " IN (-1,";
+        if ( $("#cbType1").is(':checked')) searchType += "1,";
+        if ( $("#cbType2").is(':checked')) searchType += "2,";
+        if ( $("#cbType3").is(':checked')) searchType += "3,";
+        self.whereClause += " AND " + searchType.slice(0, searchType.length -1) + ")";
         //-----end of custom filters-----
 
         self.getgeoCondition(address, function (geoCondition) {
